b297b8ececc5d8b2e35526833fee1104b1eb4d24
Dominic <andro5@web.de>
2024-08-20 10:08:13 +0200

    add testcase for input 5 and refactor method

--- src/test/java/de/neuefische/FizzBuzzTest.java
+++ src/test/java/de/neuefische/FizzBuzzTest.java
@@ -39,4 +39,26 @@ class FizzBuzzTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void play_shouldReturnFizz_whenCalledWith6(){
+        //GIVEN
+        int number = 6;
+        String expected = "Fizz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnBuzz_whenCalledWith5(){
+        //GIVEN
+        int number = 5;
+        String expected = "Buzz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
 }
