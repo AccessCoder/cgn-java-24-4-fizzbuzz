e8790cb0db0fe3a61544689830ceee4f325d71c7
Dominic <andro5@web.de>
2024-08-20 10:17:31 +0200

    add tests

--- src/test/java/de/neuefische/FizzBuzzTest.java
+++ src/test/java/de/neuefische/FizzBuzzTest.java
@@ -61,4 +61,59 @@ class FizzBuzzTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void play_shouldReturnBuzz_whenCalledWith10(){
+        //GIVEN
+        int number = 10;
+        String expected = "Buzz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnFizzBuzz_whenCalledWith15(){
+        //GIVEN
+        int number = 15;
+        String expected = "FizzBuzz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnFizzBuzz_whenCalledWith0(){
+        //GIVEN
+        int number = 0;
+        String expected = "FizzBuzz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnFizzBuzz_whenCalledWithMinus1(){
+        //GIVEN
+        int number = -1;
+        String expected = "-1";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnFizzBuzz_whenCalledWithMinus15(){
+        //GIVEN
+        int number = -15;
+        String expected = "FizzBuzz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
 }
