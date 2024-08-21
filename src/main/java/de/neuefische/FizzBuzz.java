8eae596804a7a7a911dd236e8800bd213b71d9a5
Dominic <andro5@web.de>
2024-08-21 11:24:36 +0200

    fix error

--- src/main/java/de/neuefische/FizzBuzz.java
+++ src/main/java/de/neuefische/FizzBuzz.java
@@ -12,7 +12,7 @@ public class FizzBuzz {
 
     public static String play(int number) {
         if (number % 3 == 0 && number % 5 == 0){
-            return "FizzBuz";
+            return "FizzBuzz";
         } else if (number % 5 == 0) {
             return "Buzz";
         } else if (number % 3 == 0) {
