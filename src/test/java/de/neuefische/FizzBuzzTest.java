9a39e3dc660598a3c7ea9a98f45e4329dfb42652
Dominic <andro5@web.de>
2024-08-22 15:07:41 +0200

    add parameterized Tests

--- src/test/java/de/neuefische/FizzBuzzTest.java
+++ src/test/java/de/neuefische/FizzBuzzTest.java
@@ -1,116 +1,31 @@
 package de.neuefische;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
 
 
-    @Test
-    void play_shouldReturn1_whenCalledWith1(){
-        //GIVEN
-        int number = 1;
-        String expected = "1";
-        //WHEN
-        String actual = FizzBuzz.play(number);
-        //THEN
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void play_shouldReturn4_whenCalledWith4(){
-        //GIVEN
-        int number = 4;
-        String expected = "4";
-        //WHEN
-        String actual = FizzBuzz.play(number);
-        //THEN
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void play_shouldReturnFizz_whenCalledWith3(){
-        //GIVEN
-        int number = 3;
-        String expected = "Fizz";
-        //WHEN
-        String actual = FizzBuzz.play(number);
-        //THEN
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void play_shouldReturnFizz_whenCalledWith6(){
-        //GIVEN
-        int number = 6;
-        String expected = "Fizz";
-        //WHEN
-        String actual = FizzBuzz.play(number);
-        //THEN
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void play_shouldReturnBuzz_whenCalledWith5(){
-        //GIVEN
-        int number = 5;
-        String expected = "Buzz";
-        //WHEN
-        String actual = FizzBuzz.play(number);
-        //THEN
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void play_shouldReturnBuzz_whenCalledWith10(){
-        //GIVEN
-        int number = 10;
-        String expected = "Buzz";
-        //WHEN
-        String actual = FizzBuzz.play(number);
-        //THEN
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void play_shouldReturnFizzBuzz_whenCalledWith15(){
-        //GIVEN
-        int number = 15;
-        String expected = "FizzBuzz";
-        //WHEN
-        String actual = FizzBuzz.play(number);
-        //THEN
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void play_shouldReturnFizzBuzz_whenCalledWith0(){
-        //GIVEN
-        int number = 0;
-        String expected = "FizzBuzz";
-        //WHEN
-        String actual = FizzBuzz.play(number);
-        //THEN
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void play_shouldReturnFizzBuzz_whenCalledWithMinus1(){
-        //GIVEN
-        int number = -1;
-        String expected = "-1";
-        //WHEN
-        String actual = FizzBuzz.play(number);
-        //THEN
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void play_shouldReturnFizzBuzz_whenCalledWithMinus15(){
-        //GIVEN
-        int number = -15;
-        String expected = "FizzBuzz";
+    @ParameterizedTest
+    @CsvSource(
+            value = {
+                    "1,1",
+                    "4,4",
+                    "3,Fizz",
+                    "6,Fizz",
+                    "5,Buzz",
+                    "10,Buzz",
+                    "15,FizzBuzz",
+                    "0,FizzBuzz",
+                    "-1,-1",
+                    "-15,FizzBuzz"
+            }
+    )
+    void testPlay(int number, String expected){
         //WHEN
         String actual = FizzBuzz.play(number);
         //THEN
